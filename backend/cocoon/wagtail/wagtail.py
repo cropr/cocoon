17e1a42ee0537e1569d5249e21e43e5adcfdbc60
Ruben Decrop <ruben@chessdevil.net>
2025-07-28 09:02:47 +0200

    added cache for wagtail
    wip

--- backend/cocoon/wagtail/wagtail.py
+++ backend/cocoon/wagtail/wagtail.py
@@ -3,18 +3,27 @@
 
 import logging
 import httpx
+import asyncstdlib as a
+import asyncio
 from bs4 import BeautifulSoup
 from reddevil.core import get_settings
 from reddevil.core import RdException, RdNotFound
 
 
 logger = logging.getLogger(__name__)
+clear_cache_started = False
+CACHE_EXPIRES = 300  # Cache expiration time in seconds
 
 
+@a.cache
 async def wagtail_getpages():
     """
     Lists files in Wagtail cms.
     """
+    global clear_cache_started
+    if not clear_cache_started:
+        clear_cache_started = True
+        await clear_cache()  # Start the cache clearing loop
     try:
         url = get_settings().WAGTAIL_URL
         async with httpx.AsyncClient() as client:
@@ -27,6 +36,7 @@ async def wagtail_getpages():
         raise RdException("Failed to list Wagtail files") from e
 
 
+@a.cache
 async def wagtail_getpage(slug: str):
     """
     Get file from Wagtail cms.
@@ -45,14 +55,12 @@ async def wagtail_getpage(slug: str):
             r = await client.get(f"{url}/api/pages/{id}/")
             doc = r.json()
         body = BeautifulSoup(doc["body"], "html.parser")
-        logger.info(f"before {body=}")
         for elem in body:
             if not elem.name == "embed":
                 continue
             if elem.get("embedtype") == "image":
                 async with httpx.AsyncClient() as client:
                     imagestr = await client.get(f"{url}/api/images/{elem['id']}/")
-                    logger.info(f"imagestr {imagestr}")
                 image = imagestr.json()
                 logger.info(f"image {image}")
                 elem.name = "img"
@@ -63,7 +71,6 @@ async def wagtail_getpage(slug: str):
                     "width": image["width"],
                     "height": image["height"],
                 }
-        logger.info(f"after {body=}")
         doc["body"] = str(body)
         return doc
     except httpx.HTTPStatusError as e:
@@ -73,6 +80,7 @@ async def wagtail_getpage(slug: str):
         raise RdException(f"Failed to get Wagtail file {id}") from e
 
 
+@a.cache
 async def wagtail_getimages():
     """
     Lists files in Wagtail cms.
@@ -89,6 +97,7 @@ async def wagtail_getimages():
         raise RdException("Failed to list Wagtail images") from e
 
 
+@a.cache
 async def wagtail_getimage(title: str):
     """
     Get file from Wagtail cms.
@@ -112,3 +121,20 @@ async def wagtail_getimage(title: str):
             f"HTTP error occurred: {e.response.status_code} - {e.response.text}"
         )
         raise RdException(f"Failed to get Wagtail file {id}") from e
+
+
+async def clear_cache():
+    """
+    Clear the Wagtail cache.
+    """
+    await asyncio.sleep(CACHE_EXPIRES)
+    try:
+        wagtail_getpages.cache_clear()
+        wagtail_getpage.cache_clear()
+        wagtail_getimages.cache_clear()
+        wagtail_getimage.cache_clear()
+        logger.info("Wagtail cache cleared")
+    except Exception as e:
+        logger.error(f"Failed to clear Wagtail cache: {e}")
+        raise RdException("Failed to clear Wagtail cache") from e
+    await clear_cache()  # Schedule the next cache clear
